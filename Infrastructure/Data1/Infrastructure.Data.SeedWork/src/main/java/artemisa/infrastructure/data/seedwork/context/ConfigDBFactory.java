1e9eb3ba6f9bb2bf67aa80873b73d72a6273208f
Diego <diegotl6691@gmail.com>
2019-06-22 13:02:43 -0500

    changes fix
    changes fix

--- Infrastructure/Data1/Infrastructure.Data.SeedWork/src/main/java/artemisa/infrastructure/data/seedwork/context/ConfigDBFactory.java
+++ Infrastructure/Data1/Infrastructure.Data.SeedWork/src/main/java/artemisa/infrastructure/data/seedwork/context/ConfigDBFactory.java
@@ -33,36 +33,48 @@ public class ConfigDBFactory {
 	@Bean(name="dsAcceso")
 	@ConfigurationProperties(prefix="spring.datasource.acceso")
 	public DataSource dsAcceso() {
-		
+		/*DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.postgresql.Driver");
+        dataSourceBuilder.url("jdbc:postgresql://142.44.210.9:5432/bd_acceso?autoReconnect=true&useSSL=false");
+        dataSourceBuilder.username("segAcceso");
+        dataSourceBuilder.password("artemisa");
+        return dataSourceBuilder.build();*/
 		return DataSourceBuilder.create().build();
 	}
 	
 	
-	@Bean(name="dsAcceso")
+	/*@Bean(name="dsAcceso")
 	@Primary
 	@ConfigurationProperties("spring.datasource.acceso")
 	public DataSourceProperties dataSourceProperties() {
 	    return new DataSourceProperties();
-	}
+	}*/
 	
 	@Bean(name="dsDayr")
     @ConfigurationProperties(prefix="spring.datasource.dayr")
     public DataSource dsDayr() {
+		/*DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.postgresql.Driver");
+        dataSourceBuilder.url("jdbc:postgresql://142.44.210.9:5432/bd_dayr?autoReconnect=true&useSSL=false");
+        dataSourceBuilder.username("userTransaccion");
+        dataSourceBuilder.password("artemisa");
+        return dataSourceBuilder.build();*/
         return DataSourceBuilder.create().build();
     }
 
 	@Bean(name="jdbcAcceso")
-	@Autowired
 	public JdbcTemplate createJdbcTemplateArtemisa(@Qualifier("dsAcceso") DataSource dsAcceso) {
 		return new JdbcTemplate(dsAcceso);
 	}
 	
 	@Bean(name="jdbcDayr")
-	@Autowired
 	public JdbcTemplate createJdbcTemplateDayr(@Qualifier("dsDayr") DataSource dsDayr) {
 		return new JdbcTemplate(dsDayr);
 	}
 	
-	
+	@Bean(name="objDependency")
+	public String holaMundo() {
+		return "holaMundoJava";
+	}
 	
 }
